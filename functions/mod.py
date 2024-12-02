66beae0d2910b90c4afb4d9b30a46c4387c4bffe
sky9154 <sky0932039612@gmail.com>
2024-12-02 21:40:00 +0800

    Add author extraction functionality

--- functions/mod.py
+++ functions/mod.py
@@ -1,3 +1,5 @@
+import os
+import re
 import zipfile
 import xml.etree.ElementTree as ET
 
@@ -11,8 +13,13 @@ class Mod:
       with zipmod.open('manifest.xml') as manifest:
         tree = ET.parse(manifest)
         root = tree.getroot()
+        author = os.path.basename(mod_path)
 
         for child in root:
           mod_info[child.tag] = child.text.strip() if child.text else 'None'
+
+        if mod_info['author'] == 'None':
+          if '[' in author and ']':
+            mod_info['author'] = re.search(r'\[(.*?)\]', author).group(1)
 
     return mod_info
